ba65d1a8854c1b95e6957bfa953bfa6f5b571626
Anto/236 <Antonymbugua1237@gmail.com>
2023-07-29 14:50:48 +0000

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python3
-""" Unittest module """
+"""
+Unit tests for client.GithubOrgClient class
+"""
 
-from unittest import TestCase, mock
-from unittest.mock import patch, Mock
+import unittest
+from unittest.mock import patch
+from client import GithubOrgClient
 from parameterized import parameterized
 
-import client
-from client import GithubOrgClient
 
-
-class TestGithubOrgClient(TestCase):
-    """ Class for testing GithubOrgClient """
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    TestGithubOrgClient class to test the GithubOrgClient class
+    """
 
     @parameterized.expand([
-        ("google"),
-        ("abc"),
+        ("google",),
+        ("abc",),
     ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_json):
-        """ Test method returns correct output """
-        gc = GithubOrgClient(org_name)
-        gc.org()
-        mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-            )
+    @patch('client.GithubOrgClient.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """
+        Test the GithubOrgClient.org method with mocked get_json.
+        """
+        """Configure the mock to return a specific
+        value when get_json is called."""
+        expected_result = {
+            "name": org_name,
+            "repos_url": f"https://api.github.com/orgs/{org_name}/repos"
+        }
+        mock_get_json.return_value = expected_result
+
+        """Create an instance of GithubOrgClient"""
+        github_client = GithubOrgClient(org_name)
+
+        """Call the org method"""
+        result = github_client.org()
+
+        """Check that get_json is called once with the expected argument"""
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")
+
+        """Check that the result is correct"""
+        self.assertEqual(result, expected_result)

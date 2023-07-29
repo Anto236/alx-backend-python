97eb196016ab7fdb05182dbbb0ea84a4d0a82e5a
Anto/236 <Antonymbugua1237@gmail.com>
2023-07-29 15:12:20 +0000

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,3 +23,20 @@ class TestGithubOrgClient(TestCase):
         github_client.org()
         mock_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_method):
+        '''self descriptive'''
+        payload = [{"name": "Google"}, {"name": "TT"}]
+        mocked_method.return_value = payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mocked_public:
+
+            mocked_public.return_value = "world"
+            response = GithubOrgClient('test').public_repos()
+
+            self.assertEqual(response, ["Google", "TT"])
+
+            mocked_public.assert_called_once()
+            mocked_method.assert_called_once()

77826f56944f5b1b647e5cf4efc02ad856170ac3
Anto/236 <Antonymbugua1237@gmail.com>
2023-07-29 15:14:35 +0000

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,19 +24,14 @@ class TestGithubOrgClient(TestCase):
         mock_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
 
-    @patch('client.get_json')
-    def test_public_repos(self, mocked_method):
-        '''self descriptive'''
-        payload = [{"name": "Google"}, {"name": "TT"}]
-        mocked_method.return_value = payload
-
-        with patch('client.GithubOrgClient._public_repos_url',
-                   new_callable=PropertyMock) as mocked_public:
-
-            mocked_public.return_value = "world"
-            response = GithubOrgClient('test').public_repos()
-
-            self.assertEqual(response, ["Google", "TT"])
-
-            mocked_public.assert_called_once()
-            mocked_method.assert_called_once()
+    @parameterized.expand([
+        ('random_url', {'repos_url': 'http://some_url.com'})
+    ])
+    def test_public_repos_url(self, name, result):
+        """
+        self descriptive
+        """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get('repos_url'))
